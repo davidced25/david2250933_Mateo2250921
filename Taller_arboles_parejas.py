cd25b45492ded4eb452f14cbeedb84765295b505
davidced25 <david2250933@correo.uis.edu.co>
2026-04-07 18:28:33 -0500

    Update Taller_arboles_parejas.py

--- Taller_arboles_parejas.py
+++ Taller_arboles_parejas.py
@@ -1,5 +1,14 @@
 class nodo:
     def __init__(self, dato):
         self.dato = dato
-        self.izquierdo = None
-        self.derecho = None
+        self.padre = None
+class arbol:
+    def __init__(self):
+        self.raiz = None
+    def insertar_nodo(self,dato)
+        nuevo = nodo(valor)
+        if self.raiz is None:
+            self.raiz = nuevo
+            return
+
+    
